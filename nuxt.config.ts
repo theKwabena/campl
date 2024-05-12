51868be950b8bc691fed02adc669e77162c7fe40
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-05-12 18:32:28 +0000

    added oauth login for github

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    'nuxt-svgo',
-    '@vueuse/nuxt'
-  ],
+  modules: ['nuxt-svgo', '@vueuse/nuxt', "@nuxtjs/supabase"],
   svgo: {
     autoImportPath: false
   },
