340a2795b25d4a59f028c22819aa151f1d8ecd80
theKwabena <abbeyemmanueltetteh@gmail.com>
2024-05-18 13:23:15 +0000

    updated useFetch to use cache

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,14 +1,33 @@
-export const useLesson = async ( courseSlug:string, chapterSlug:string, lessonSlug:string) => {
-  const {data, error } = useFetch(
-      `/api/courses/${courseSlug}/chapters/${chapterSlug}/lessons/${lessonSlug}`
-  );
+import type {Lesson} from "~/types/course";
+import {StorageSerializers} from "@vueuse/core";
+import {da} from "cronstrue/dist/i18n/locales/da";
 
-  if(error.value){
-    throw createError({
-      ...error.value,
-      statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug} of course ${courseSlug}`
-    })
+export const useLesson = async ( courseSlug:string, chapterSlug:string, lessonSlug:string) => {
+
+  // cache lesson data
+  const url = `/api/courses/${courseSlug}/chapters/${chapterSlug}/lessons/${lessonSlug}`
+  const lesson = useSessionStorage<Lesson>(
+      url,
+      null,
+      {
+        // passing null up there means we need to specify the serializer to use
+        // we can actually use {} and remove this curly brace
+        serializer: StorageSerializers.object
+      }
+  )
+
+  if (!lesson.value){
+    const {data, error } = await useFetch<Lesson>(url);
+
+    if(error.value){
+      throw createError({
+        ...error.value,
+        statusMessage: `Could not fetch lesson ${lessonSlug} in chapter ${chapterSlug} of course ${courseSlug}`
+      })
+    }
+
+    lesson.value = data.value
   }
 
-  return data
+  return lesson
 }
